9c9171f141c1eaee68ce7c30c892e1d535f2b751
Felix Volkmer <noninfelix@gmail.com>
2023-08-19 14:12:24 +0200

    add styling

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
 <template>
-  <main></main>
+  <main>
+    <header>
+      <img src="./assets/pinia-logo.svg" alt="" />
+      <h1>Pinia Tasks</h1>
+    </header>
+  </main>
 </template>
 
 <script>
 export default {};
 </script>
+<style>
+@import url("./assets/main.css");
+</style>
