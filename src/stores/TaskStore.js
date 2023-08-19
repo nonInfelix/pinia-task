13de9aa92d1f6f1ce2e5273e7e54f60bdc807325
Felix Volkmer <noninfelix@gmail.com>
2023-08-19 14:32:06 +0200

    create pinia store for tasks

---
+++ src/stores/TaskStore.js
@@ -0,0 +1,12 @@
+import { defineStore } from "pinia";
+
+// erstellt neuen Store, speichert in Variable und exportiert diesen
+export const useTaskStore = defineStore("tasks", {
+  state: () => ({
+    // task state
+    tasks: [
+      { id: 1, title: "example task 1", isFav: false },
+      { id: 2, title: "example task 2", isFav: true },
+    ],
+  }),
+});
